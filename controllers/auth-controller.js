5cf19a8d73d54dbed7c667927a9c04d9be24bab5
yjjy94 <yoheej94@gmail.com>
2022-02-26 17:22:50 +0900

    Create auth-controller.js

---
+++ controllers/auth-controller.js
@@ -0,0 +1,55 @@
+const User = require("../models/users-model");
+
+function getSignUpView(req, res) {
+  res.render("user/signup");
+}
+
+async function signUp(req, res, next) {
+  const data = req.body;
+  try {
+    const existingUser = await user.checkEmail();
+    if (existingUser) {
+      console.log("User name already in use");
+      res.redirect("user/signup");
+      return;
+    }
+
+    await user.newSignUp();
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+
+  res.redirect("/login");
+}
+
+function getLogInView(req, res) {
+  res.render("user/login");
+}
+
+async function logIn(req, res, next) {
+  // console.log("LOGIN IS " + res.locals.csrfToken);
+  const user = new User(req.body.email, req.body.password);
+  let existingUser;
+
+  try {
+    existingUser = await user.checkEmail();
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+
+  if (!existingUser) {
+  }
+  //no need for try catch here cuz checkPW is not connecting to db so most likely wont fail
+  const checkPassword = await user.checkPassword(existingUser.password);
+  if (!checkPassword) {
+  }
+}
+
+module.exports = {
+  getSignUpView: getSignUpView,
+  getLogInView: getLogInView,
+  signUp: signUp,
+  logIn: logIn,
+};
